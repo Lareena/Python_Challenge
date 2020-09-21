f19b2b0484e11b8ae3f45bb4b173444c5726132e
Lareena <lareena.e.llamado@gmail.com>
2020-09-20 21:01:14 -0400

    update main

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,43 +3,87 @@ import os
 import csv
 
 #specify file path
-csvpath = os.path.join(".","PyBank","budget_data3.csv")
+csvpath = os.path.join("..","PyBank","budget_data3.csv")
+
+with open("budget_data3.csv","r") as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter = ",")
+    csv_reader =next(csv_reader)
+    output_file = "budget_analysis.txt"
 
 #initialize variables
-total_revenue = 0
-total_months = 0
-revenue_length = 0
-avg_revenue_change = 0
+    revenue_length = 0
+    avg_revenue_change = 0
+    max_revenue_change = 0
+    min_revenue_change = 0
+    revenue = []
+    revenue_change = []
+    months = []
 
 
-
-
-with open (csvpath) as csvfile:
-
-    csvreader=csv.reader(csvfile, delimiter=',')
-    
-    print(csvreader)
-
-print("-------------------------------------------------------------------")
 #total number of months included in dataset
-with row in csvfile:
-    total_months = total_revenue + 1
+    for row in csv_reader:
+    
+        total_months = 0
+        total_months = total_months + 1
 
-    total_revenue = total_revenue + int(row[1])
-
-   
-
-print("-------------------------------------------------------------------")
+    
 #net total amount of "Profit/Losses" over the entire period
+   
+        total_revenue = 0
+        total_revenue = total_revenue + int(row[1])
+        revenue.append(int(row[1]))
+        months.append(int(row[0]))
+        
 
 
-print("-------------------------------------------------------------------")
 #average of the changes in "Profit/Losses" over the entire period
+    for i in range(len(revenue)-1):
+        change = int(revenue[i+1]) - int(revenue[i])
+        
+        avg_revenue_change = int(revenue[i+1]) - int(revenue[i])
+
+        avg_revenue_change = abs(sum(revenue_change)/len(revenue_change))
+        
 
 
-print("-------------------------------------------------------------------")
+
 #the greatest increase in profits (date and amount) over the entire period
 
+        
+        max_revenue_change= max(revenue_change)
+        max_revenue_change_date = str(date[revenue_change.index(max(revenue_change))])
 
-print("-------------------------------------------------------------------")
 #the greatest decrease in losses (date and amount) over the entire period
+        min_revenue_change= min(revenue_change)
+        min_revenue_change_date = str(date[revenue_change.index(min(revenue_change))])
+
+
+
+#Print the outputs
+        print("Financial Analysis")
+        print("--------------------")
+
+        print(f"Total Months:  {str(total_months)}")
+        print(f"Total Revenue:  {str(total_revenue)}")
+        print(f"Average Revenue Change + {str(avg_revenue_change)}")
+        print(f"Greatest Increase in Revenue:  {str(max_revenue_change_date) +str(max_revenue_change)}")
+        print(f"Greatest Decrease in Revenue:  {str(min_revenue_change_date) +str(min_revenue_change)}")
+
+#Output to text file
+output_file = os.path.join("output.csv")
+with open(file_to_output,"w") as datafile:
+    writer = csv.writer(datafile)
+    datafile.write("Financial Analysis")
+    datafile.write("\n")
+    datafile.write(f"Total Months:  {str(total_months)}")
+    datafile.write("\n")
+    datafile.write(f"Total Revenue:  {str(total_revenue)}")
+    datafile.write("\n")
+    datafile.write(f"Average Revenue Change + {str(avg_revenue_change)}")
+    datafile.write("\n")
+    datafile.write((f"Greatest Increase in Revenue:  {str(max_revenue_change_date) +str(max_revenue_change)}")
+    datafile.write("\n")
+    datafile.write(f"Greatest Decrease in Revenue:  {str(min_revenue_change_date) +str(min_revenue_change)}")
+
+
+
