f93496918af02d3494742cf004ca2bb69cd78213
Lareena <lareena.e.llamado@gmail.com>
2020-09-29 21:03:06 -0400

    another commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,8 +8,8 @@ csvpath = os.path.join("budget_data3.csv")
 #open csvfile
 with open("budget_data3.csv","r") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter = ",")
-    csv_reader = next(csv_reader)
-    file_to_output = "Budget_analysis.txt"
+    csv_header = next(csv_reader)
+    
 
 #initialize variables
     
@@ -17,63 +17,57 @@ with open("budget_data3.csv","r") as csvfile:
     revenue_change = []
     months = []
     monthly_change = []
+    total_months = 0
 
 #total number of months included in dataset
     for row in csv_reader:
         
-        months.append(int(row[0]))
+        months.append((row[0]))
         revenue.append(int(row[1]))
         
         total_months = total_months + 1
         
        
     
-#net total amount of "Profit/Losses" over the entire period
-       
-        total_revenue = 0
-        total_revenue = sum(revenue)
-        
-        
-        
-
-
-#average of the changes in "Profit/Losses" over the entire period
+#net total amount of "Profit/Losses" over the entire period    
+    total_revenue = 0
+    total_revenue = sum(revenue)
+    
     for i in range(len(revenue)-1):
+            #i = 0   
+            #change = int(revenue[i+1]) - int(revenue[i])
         
-        i = 0   
-        change = int(revenue[i+1]) - int(revenue[i])
-        
-        avg_revenue_change = int(revenue[i+1]) - int(revenue[i])
+            revenue_change = int(revenue[i+1]) - int(revenue[i])
 
-        avg_revenue_change = abs(sum(revenue_change)/len(revenue_change))
-        
-
-
-
-#the greatest increase in profits (date and amount) over the entire period
-
-        
-        max_revenue_change= max(revenue_change)
-        max_revenue_change_date = str(months[revenue_change.index(max(revenue_change))])
-
-#the greatest decrease in losses (date and amount) over the entire period
-        min_revenue_change= min(revenue_change)
-        min_revenue_change_date = str(months[revenue_change.index(min(revenue_change))])
+            avg_revenue_change = abs(sum(revenue_change)/len(revenue_change))
+            #average of the changes in "Profit/Losses" over the entire period
+            # #range(len(list)) : 0,1,2,3,4, ... ,len(list) 
+            # 
+            # #the greatest increase in profits (date and amount) over the entire period
+            # 
+            max_revenue_change= max(revenue_change)
+            #max_revenue_change_date = str(months[revenue_change.index(max(revenue_change))])
+            #the greatest decrease in losses (date and amount) over the entire period
+            # min_revenue_change= min(revenue_change)
+        #min_revenue_change_date = str(months[revenue_change.index(min(revenue_change))])
 
 
 
 #Print the outputs
-        print("Budget Analysis")
-        print("--------------------")
+    print("Budget Analysis")
+    print("--------------------")
 
-        print(f"Total Months:  {str(total_months)}")
-        print(f"Total Revenue:  {str(total_revenue)}")
-        print(f"Average Revenue Change + {str(avg_revenue_change)}")
-        print(f"Greatest Increase in Revenue:  {str(max_revenue_change_date) +str(max_revenue_change)}")
-        print(f"Greatest Decrease in Revenue:  {str(min_revenue_change_date) +str(min_revenue_change)}")
+    print(f"Total Months:  {str(total_months)}")
+    print(f"Total Revenue:  {str(total_revenue)}")
+    print(f"Average Revenue Change + {str(avg_revenue_change)}")
+        #print(f"Greatest Increase in Revenue:  {str(max_revenue_change_date) +str(max_revenue_change)}")
+        #print(f"Greatest Decrease in Revenue:  {str(min_revenue_change_date) +str(min_revenue_change)}")
 
 #Output to text file
 output_file = os.path.join("output.csv","new.csv")
+
+file_to_output = "Budget_analysis.txt"
+
 with open(file_to_output,"w") as datafile:
     csvwriter = csv.writer(datafile)
     
@@ -81,8 +75,8 @@ with open(file_to_output,"w") as datafile:
     datafile.writerow(f"Total Months:  {str(total_months)}")
     datafile.writerow(f"Total Revenue:  {str(total_revenue)}")
     datafile.writerow(f"Average Revenue Change + {str(avg_revenue_change)}")
-    datafile.writerow(f"Greatest Increase in Revenue:  {str(max_revenue_change_date) +str(max_revenue_change)}")
-    datafile.writerow(f"Greatest Decrease in Revenue:  {str(min_revenue_change_date) +str(min_revenue_change)}")
+    #datafile.writerow(f"Greatest Increase in Revenue:  {str(max_revenue_change_date) +str(max_revenue_change)}")
+    #datafile.writerow(f"Greatest Decrease in Revenue:  {str(min_revenue_change_date) +str(min_revenue_change)}")
 
 
 
